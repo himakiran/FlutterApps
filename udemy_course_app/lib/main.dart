93d0b3255341394074cfdf1fd74d4f9c832017e9
himakiran <himakiran@chundi.in>
2019-06-09 15:33:03 +0530

    Used a ListView
    Used a ListView instead of column to avoid the screen overflow error

--- udemy_course_app/lib/main.dart
+++ udemy_course_app/lib/main.dart
@@ -13,18 +13,20 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(title: Text("EasyList"), backgroundColor: Colors.red),
         body: Card(
-          child: Column(
-            children: <Widget>[
+          child: ListView(
+              shrinkWrap: true,
+              padding: EdgeInsets.all(15.0),
+              children: <Widget>[
               Image.asset('assets/food.jpg'),
               Text('Food Paradise'),
               Image.asset('assets/food.jpg'),
               Text('Food Paradise 2'),
               Image.asset('assets/food.jpg'),
               Text('Food Paradise 3')
-            ],
+            ], 
           ),
-        ),
       ),
+    ),
     );
   }
 }
