44b78b69fa3ecdb9c2ea68bdb12af102e297f49d
himakiran <himakiran@chundi.in>
2019-06-09 18:07:24 +0530

    Implemented stateful widget
    Now the add button on being pressed adds a new card.

--- udemy_course_app/lib/main.dart
+++ udemy_course_app/lib/main.dart
@@ -7,43 +7,70 @@ import 'package:flutter/material.dart';
 void main() => runApp(
     MyApp()); // This syntax is valid if main() contains only one statement
 
-class MyApp extends StatelessWidget {
+// dynamic apps require StatefulWidget
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+// The underscore before the class name means that his class
+// cannot be imported into another file and is to be used here only
+
+class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Food Tester'];
+  @override
   Widget build(BuildContext context) {
+    // Meaning of below line :
+    // The array/list _products is mapped to a function .
+    //  This function wich is not named and is written wholly inside
+    //  accepts each element from the list and converts it into the card
+    //  as shown below. The output of the function is a list of card widgets
+
+    // _products.map((element) => Card(
+    //       child: Column(
+    //         children: <Widget>[
+    //           Image.asset('assets/food.jpg'),
+    //           Center(child: Text(element)),
+    //         ],
+    //       ),
+    //     ));
+
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(title: Text("EasyList"), backgroundColor: Colors.red),
-          body: ListView(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.all(10.0),
-                alignment: Alignment.center,
-                child: RaisedButton(
-                  onPressed: () {},
-                  child: Text('ADD'),
-                ),
-              ),
-              Card(
-                child: ListView(
-                  shrinkWrap: true,
-                  padding: EdgeInsets.all(10.0),
-                  children: <Widget>[
-                    Image.asset('assets/food.jpg'),
-                    Center(child:Text('Food Paradise')),
-                    Image.asset('assets/food.jpg'),
-                    Center(child:Text('Food Paradise')),
-                    Image.asset('assets/food.jpg'),
-                    Center(child:Text('Food Paradise')),
-                    Image.asset('assets/food.jpg'),
-                    Center(child:Text('Food Paradise')),
-                    Image.asset('assets/food.jpg'),
-                    Center(child:Text('Food Paradise')),
-                    Image.asset('assets/food.jpg'),
-                    Center(child:Text('Food Paradise'))
-                  ],
-                ),
-              ),
-            ],
-          )),
-    );
+        home: Scaffold(
+      appBar: AppBar(title: Text("EasyList"), backgroundColor: Colors.red),
+      body: ListView(children: <Widget>[
+        Container(
+          margin: EdgeInsets.all(10.0),
+          alignment: Alignment.center,
+          child: RaisedButton(
+            onPressed: () {
+              setState(() {      // Any code which changes the internal state has to be put here in this function for it to work.
+                _products.add('Advanced Food Tester');
+              });
+              
+            },
+            child: Text('ADD'),
+          ),
+        ),
+        Column(
+          // Read the explanation of the below line at the start of this class
+          children: _products
+              .map((element) => Card(
+                    child: ListView(
+                      shrinkWrap: true,
+                      padding: EdgeInsets.all(10.0),
+                      children: <Widget>[
+                        Image.asset('assets/food.jpg'),
+                        Center(child: Text(element)),
+                      ],
+                    ),
+                  ))
+              .toList(),
+        )
+      ]),
+    ));
   }
 }
---
+++ udemy_course_app/lib/main_stateless.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+// main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(
+    MyApp()); // This syntax is valid if main() contains only one statement
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(title: Text("EasyList"), backgroundColor: Colors.red),
+          body: ListView(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.all(10.0),
+                alignment: Alignment.center,
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text('ADD'),
+                ),
+              ),
+              Card(
+                child: ListView(
+                  shrinkWrap: true,
+                  padding: EdgeInsets.all(10.0),
+                  children: <Widget>[
+                    Image.asset('assets/food.jpg'),
+                    Center(child:Text('Food Paradise')),
+                    Image.asset('assets/food.jpg'),
+                    Center(child:Text('Food Paradise')),
+                    Image.asset('assets/food.jpg'),
+                    Center(child:Text('Food Paradise')),
+                    Image.asset('assets/food.jpg'),
+                    Center(child:Text('Food Paradise')),
+                    Image.asset('assets/food.jpg'),
+                    Center(child:Text('Food Paradise')),
+                    Image.asset('assets/food.jpg'),
+                    Center(child:Text('Food Paradise'))
+                  ],
+                ),
+              ),
+            ],
+          )),
+    );
+  }
+}
