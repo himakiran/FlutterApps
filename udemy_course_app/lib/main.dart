c804e95c7f6582e45c5a16180273d4b07c686beb
himakiran <himakiran@chundi.in>
2019-06-09 14:35:16 +0530

    added a card widget
    using a card widget inside the scaffold

--- udemy_course_app/lib/main.dart
+++ udemy_course_app/lib/main.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
 
+// main() {
+//   runApp(MyApp());
+// }
 
-main() {
-  runApp(MyApp());
-}
+void main() => runApp(
+    MyApp()); // This syntax is valid if main() contains only one statement
 
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("EasyList"),
-          backgroundColor: Colors.red
+        appBar: AppBar(title: Text("EasyList"), backgroundColor: Colors.red),
+        body: Card(
+          child: Column(
+            children: <Widget>[
+              Image.asset('assets/food.jpg'),
+              Text('Food Paradise')
+            ],
+          ),
         ),
       ),
     );
