f9e9d8b863c0093085c2f551c2b4d808ee7bf1bb
himakiran <himakiran@chundi.in>
2019-06-15 12:13:12 +0530

    Create product_control.dart
    This file illusrtrates how we can have accessto a state setting function in a class that extends the statelesss widget

---
+++ udemy_course_app/lib/product_control.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ProductControl extends StatelessWidget {
+
+  // GETTING ACCESS TO STATE SETTING FUNCTION IN STATELESS WIDGET ::
+
+  // as this is a statelesswidget to have setstate for onPressed in button
+  // i need access to a state setting function.
+  // The state setting function is present in product_manager.dart which
+  // extends a stateful widget. To get that function here we define a final Function below
+  // and set it to the reference we receive in the constructor
+
+  final Function addProduct;
+  ProductControl(this.addProduct);
+  @override
+  Widget build(BuildContext context) {
+    
+    return RaisedButton(
+        color: Theme.of(context).primaryColor,
+        onPressed: () {
+          // Now the below function will actually run the _addProduct function
+          // in the product_manager.dart file
+          addProduct();
+        },
+        child: Text('ADD'),
+      );
+  }
+  
+}
