efa3b9d21a5147aa5a75368acb86dc5c5e111e6d
himakiran <himakiran@chundi.in>
2019-06-19 00:05:56 +0530

    Update color_state.dart
    formating and added comments

--- colors_app/lib/color_state.dart
+++ colors_app/lib/color_state.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'dart:math';
 import 'package:quiver/iterables.dart';
 
+import 'dart:math';
 
 class ColorState extends StatefulWidget {
   @override
@@ -33,26 +33,30 @@ class _ColorState extends State<ColorState> {
     Colors.brown,
     Colors.blueGrey
   ];
+  // This list defines the primary materialcolors
   static List<String> colorNames = const <String>[
     "Red",
     "Pink",
     "Purple",
-      "DeepPurple",
-      "Indigo",
-      "Blue",
-      "LightBlue",
-      "Cyan",
-      "Teal",
-      "Green",
-      "LightGreen",
-      "Lime",
-      "Yellow",
-      "Amber",
-      "Orange",
-      "DeepOrange",
-      "Brown",
-      "BlueGrey"
+    "DeepPurple",
+    "Indigo",
+    "Blue",
+    "LightBlue",
+    "Cyan",
+    "Teal",
+    "Green",
+    "LightGreen",
+    "Lime",
+    "Yellow",
+    "Amber",
+    "Orange",
+    "DeepOrange",
+    "Brown",
+    "BlueGrey"
   ];
+
+  // Material Accent Colors
+
   static List<MaterialAccentColor> accentColorsDisplayed =
       const <MaterialAccentColor>[
     Colors.redAccent,
@@ -72,92 +76,95 @@ class _ColorState extends State<ColorState> {
     Colors.orangeAccent,
     Colors.deepOrangeAccent
   ];
+
+  // material accent colors names 
   static List<String> accentColorNames = const <String>[
     "RedAccent",
     "PinkAccent",
     "PurpleAccent",
-      "DeepPurpleAccent",
-      "IndigoAccent",
-      "BlueAccent",
-      "LightBlueAccent",
-      "CyanAccent",
-      "TealAccent",
-      "GreenAccent",
-      "LightGreenAccent",
-      "LimeAccent",
-      "YellowAccent",
-      "AmberAccent",
-      "OrangeAccent",
-      "DeepOrangeAccent",
-      "BrownAccent",
-      "BlueGreyAccent"
+    "DeepPurpleAccent",
+    "IndigoAccent",
+    "BlueAccent",
+    "LightBlueAccent",
+    "CyanAccent",
+    "TealAccent",
+    "GreenAccent",
+    "LightGreenAccent",
+    "LimeAccent",
+    "YellowAccent",
+    "AmberAccent",
+    "OrangeAccent",
+    "DeepOrangeAccent",
+    "BrownAccent",
+    "BlueGreyAccent"
   ];
   // This variable holds the color which shall be painted
   Color showColor;
-  // This variable holds the name of the material color
+  // This variable holds the name of the material color painted
   String showColorName;
 
   // To create a list of all material colors, we define a function listAllColors
-  // This function takes in the list of primary material colors and returns the colorswatch of that color
-  // and the colorswatch of the accent color.
+  // This function takes in the list of primary material colors, accentcolors and two lists of names of each
+  // and fills in the two lists below
 
   List<Color> allColors = new List<Color>();
-    List<String> allColorsNames = new List<String>();
-  
-  void listAllColors(
-      List<MaterialColor> ll, List<MaterialAccentColor> al, List<String> cl, List<String> ca) {
+  List<String> allColorsNames = new List<String>();
 
-    
-
-    // This function adds all the swatch colors
+  void listAllColors(List<MaterialColor> ll, List<MaterialAccentColor> al,
+      List<String> cl, List<String> ca) {
+    // This function adds all the swatch colors and their names
     void addMaterialColors(MaterialColor c, String s) {
       allColors.add(c[50]); // This adds for eg Colors.red[50] to the list
-      allColorsNames.add("Colors."+s+"[50]");
+      allColorsNames.add("Colors." + s + "[50]");
       for (var j = 100; j < 1000; j += 100) {
         if (j != 500) {
           allColors.add(c[j]);
-          allColorsNames.add("Colors."+s+"["+j.toString()+"]");
+          allColorsNames.add("Colors." + s + "[" + j.toString() + "]");
         } // since the color is same as the colorswatch with index 500
         else {
           allColors.add(c);
-          allColorsNames.add("Colors."+s);
+          allColorsNames.add("Colors." + s);
         }
       }
     }
 
     // zip is from the quiver package which lets me iterate thru two lists in parallel
     for (var pair in zip([ll, cl])) {
-     addMaterialColors(pair[0],pair[1]);
+      addMaterialColors(pair[0], pair[1]);
     }
-    //ll.forEach((el) => addMaterialColors(el));
     
+    // NOT USED ANYMORE : ll.forEach((el) => addMaterialColors(el));
+
     void addMaterialAccentColors(MaterialAccentColor ac, String s) {
       allColors.add(ac[100]);
-      allColorsNames.add("Colors."+s+"[100]");
+      allColorsNames.add("Colors." + s + "[100]");
       allColors.add(ac);
-      allColorsNames.add("Colors."+s);
+      allColorsNames.add("Colors." + s);
       allColors.add(ac[400]);
-      allColorsNames.add("Colors."+s+"[400]");
+      allColorsNames.add("Colors." + s + "[400]");
       allColors.add(ac[700]);
-      allColorsNames.add("Colors."+s+"[700]");
+      allColorsNames.add("Colors." + s + "[700]");
     }
 
-    //al.forEach((el) => addMaterialAccentColors(el));
+    //NOT USED ANYMORE : al.forEach((el) => addMaterialAccentColors(el));
+
     for (var pair in zip([al, ca])) {
-     addMaterialAccentColors(pair[0],pair[1]);
+      addMaterialAccentColors(pair[0], pair[1]);
     }
   }
 
-  
-  
   @override
   void initState() {
     super.initState();
-    listAllColors(colorsDisplayed, accentColorsDisplayed,colorNames,accentColorNames);
+    // Firstly we initialize the lists
+    listAllColors(
+        colorsDisplayed, accentColorsDisplayed, colorNames, accentColorNames);
+    // Set the start color and name 
     showColor = Colors.red;
     showColorName = "Colors.red";
   }
 
+  // This functions picks a random color from the allColors list.
   void changeColor() {
     int i = 0;
     int maxSize = allColors.length;
@@ -165,11 +172,12 @@ class _ColorState extends State<ColorState> {
       //print("set state running");
       var rng = new Random();
       i = rng.nextInt(maxSize);
-
       showColor = allColors[i];
       showColorName = allColorsNames[i];
     });
   }
+
+//  This function shows the color info when the info float btn is pressed
   void showColorInfo(BuildContext context) {
     showDialog(
       context: context,
@@ -197,40 +205,34 @@ class _ColorState extends State<ColorState> {
     //print("build running");
     return MaterialApp(
         home: Scaffold(
-            body: Builder(builder: (context) => Container(
-                color: showColor,
-                child: Stack(
-                  children: <Widget>[
-                    Align(
-                      alignment: Alignment.bottomLeft,
-                      child: FloatingActionButton(
-                        onPressed: () {
-                          showColorInfo(context);
-                        },
-                        tooltip: 'Show Color Info',
-                        child: Icon(Icons.info),
-                      ),
-                    ),
-                    Align(
-                      alignment: Alignment.bottomRight,
-                      child: FloatingActionButton(
-                        onPressed: () {
-                          changeColor();
-                        },
-                        tooltip: 'Change Color',
-                        child: Icon(Icons.colorize),
-                      ),
-                    ),
-                  ],
-                )))
-            // floatingActionButton: FloatingActionButton(
-            //   onPressed: () {
-            //     changeColor();
-            //   },
-            //   tooltip: 'Change Color',
-            //   child: Icon(Icons.colorize),
-            // ),
-            // floatingActionButtonLocation: FloatingActionButtonLocation.endFloat
+            body: Builder(
+                builder: (context) => Container(
+                    color: showColor,
+                    child: Stack(
+                      children: <Widget>[
+                        Align(
+                          alignment: Alignment.bottomLeft,
+                          child: FloatingActionButton(
+                            onPressed: () {
+                              showColorInfo(context);
+                            },
+                            tooltip: 'Show Color Info',
+                            child: Icon(Icons.info),
+                          ),
+                        ),
+                        Align(
+                          alignment: Alignment.bottomRight,
+                          child: FloatingActionButton(
+                            onPressed: () {
+                              changeColor();
+                            },
+                            tooltip: 'Change Color',
+                            child: Icon(Icons.colorize),
+                          ),
+                        ),
+                      ],
+                    )))
+            
 
             ));
   }
