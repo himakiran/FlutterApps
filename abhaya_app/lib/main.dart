de84cfb16085fa5d961a3d1b2187d18ce2708318
himakiran <himakiran@chundi.in>
2019-09-07 23:49:51 +0530

    Update main.dart
    Text field with FAB impl.

--- abhaya_app/lib/main.dart
+++ abhaya_app/lib/main.dart
@@ -2,43 +2,48 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(AbhayApp());
 
-class AbhayApp extends StatelessWidget{
+class AbhayApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    
-    return MaterialApp(home:DefaultTabController(
+    return MaterialApp(
+        home: DefaultTabController(
       length: choices.length,
       child: Scaffold(
         appBar: AppBar(
           title: const Text("Crypt-Decrypt"),
           bottom: TabBar(
             isScrollable: true,
-            tabs: choices.map((Choice choice){
-            return Tab(
-              text: choice.title,
-              icon: Icon(choice.icon),
-            );             
+            tabs: choices.map((Choice choice) {
+              return Tab(
+                text: choice.title,
+                icon: Icon(choice.icon),
+              );
             }).toList(),
           ),
-
         ),
         body: TabBarView(
-          children: choices.map((Choice choice){
+          children: choices.map((Choice choice) {
             return Padding(
               padding: const EdgeInsets.all(16.0),
-              child: ChoiceCard(choice: choice),);
+              child: ChoiceCard(choice: choice),
+            );
           }).toList(),
-        ),   ),
+        ),
+      ),
     ));
   }
-  
 }
-class Choice {
-  const Choice({this.title, this.icon});
 
-  final String title;
-  final IconData icon;
+class Choice {
+  Choice({this.title, this.icon, this.tf, this.ts,this.textvalue});
+
+  String title;
+  IconData icon;
+  TextField tf;
+  TextStyle ts;
+  String textvalue;
 }
+
 class ChoiceCard extends StatelessWidget {
   const ChoiceCard({Key key, this.choice}) : super(key: key);
 
@@ -46,7 +51,7 @@ class ChoiceCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final TextStyle textStyle = Theme.of(context).textTheme.display1;
+    //final TextStyle textStyle = Theme.of(context).textTheme.display1;
     return Card(
       color: Colors.white,
       child: Center(
@@ -54,15 +59,37 @@ class ChoiceCard extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Icon(choice.icon, size: 128.0, color: textStyle.color),
-            Text(choice.title, style: textStyle),
+            choice.tf,FloatingActionButton(onPressed: () {},)
           ],
         ),
       ),
     );
   }
 }
-const List<Choice> choices = const <Choice>[
-  const Choice(title: 'ClearText', icon: Icons.folder_open),
-  const Choice(title: 'CryptText', icon: Icons.folder),
+
+ List<Choice> choices =  <Choice>[
+  Choice(
+      title: 'ClearText',
+      icon: Icons.folder_open,
+      textvalue: "",
+      tf: TextField(
+        autofocus: true,
+        style: TextStyle(color: Colors.blue, fontWeight: FontWeight.w300,fontSize: 21,),
+        decoration: InputDecoration(
+            border: InputBorder.none, hintText: 'Enter ClearText'),
+            
+      )),
+  Choice(
+      title: 'CryptText',
+      icon: Icons.folder,
+      textvalue: "",
+      tf: TextField(
+        autofocus: true,
+        style: TextStyle(color: Colors.red, fontWeight: FontWeight.w300, fontSize: 21,),
+        
+        decoration: InputDecoration(
+            border: InputBorder.none, hintText: 'Paste CryptText'),
+            
+      ),
+      ),
 ];
